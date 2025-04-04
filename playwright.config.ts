4ddec59b372648e7eecb6dd3cea5f4ce1f75ccf8
Clarissa Ortiaga <cortiaga@attune.gshapps.net>
2025-04-04 19:54:12 +0800

    add additional report formats

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,11 @@ export default defineConfig<TestOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]],
+  reporter:[
+    ['list'],
+    ['html', { open: 'never' }],
+    ['junit', {outputFile: 'test-results/results.xml'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,7 +37,8 @@ export default defineConfig<TestOptions>({
     trace: 'retain-on-first-failure',
     video: 'retain-on-failure',
     screenshot: 'on', // Or 'on' to capture for every test
-  },
+  },  
+  globalTimeout: 30000,
 
   /* Configure projects for major browsers */
   projects: [
