81ed42570d4a93b77f0e91cb47c419e42dd82d3f
Clarissa Ortiaga <cortiaga@attune.gshapps.net>
2025-04-04 19:56:18 +0800

    added additional reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig<TestOptions>({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[
+    ['github'],
     ['list'],
     ['html', { open: 'never' }],
     ['junit', {outputFile: 'test-results/results.xml'}]
